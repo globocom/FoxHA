f3b5c7ad45e30fb9f7d77266a92c97c2791a7497
otherpirate <mauro_murari@hotmail.com>
2017-03-17 15:11:41 -0300

    Filtering only enabled nodes to switch/failover

--- foxha/node_utils.py
+++ foxha/node_utils.py
@@ -2,7 +2,15 @@ from query import Query
 from node import Node
 
 
-def get_all_nodes(group, connection):
-    result = connection.execute(Query.SQL_LIST_NODES % group)
+def _get_nodes_by_query(query, group, connection):
+    result = connection.execute(query % group)
     return [Node(group, node['node_ip'], connection)
             for node in result]
+
+
+def get_all_nodes(group, connection):
+    return _get_nodes_by_query(Query.SQL_LIST_NODES, group, connection)
+
+
+def get_enabled_nodes(group, connection):
+    return _get_nodes_by_query(Query.SQL_LIST_NODES_ENABLED, group, connection)
